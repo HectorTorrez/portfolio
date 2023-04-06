2178063b701468cb0561dad6ebcd3a3f3c085b78
Hector <torrezhectorb@gmail.com>
2023-04-06 13:58:44 -0600

    fix paragraph

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
         <p>Hello,</p>
         <h1>I'm <span>Héctor</span></h1>
         {/* <p>I consider myself to be hard working, willing to adapt in general with a great passion for learning, and keep improving as a Software Developer.</p> */}
-        <p>I am a Computer Engineering student with experience in the creation of web applications with front-end technologies. Passionate about the design and development of attractive and functional user interfaces, I improve my skills in this field through various personal and academic projects, during my training, I acquired knowledge in HTML, CSS, HTML, and CSS. And JavaScript, as well as in the use of frameworks such as ReactJs.</p>
+        <p>I am a Computer Engineering student with experience in the creation of web applications with front-end technologies. Passionate about the design and development of attractive and functional user interfaces, I improve my skills in this field through various personal and academic projects, during my training, I acquired knowledge in HTML, CSS, and JavaScript, as well as in the use of frameworks such as ReactJs.</p>
         <div className='socials'>
           <Socials />
 
