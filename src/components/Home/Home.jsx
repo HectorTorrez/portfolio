c023acd9ef864fe410c8941e49d7c36bd9964164
Hector <torrezhectorb@gmail.com>
2023-08-10 13:32:22 -0600

    change resume

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,43 @@
-import { useContext } from 'react'
-import { Socials } from '../Socials'
-import './'
-import { UserContext } from '../Context/UseContext'
+import { useContext } from "react";
+import { Socials } from "../Socials";
+import "./";
+import { UserContext } from "../Context/UseContext";
 
 export const Home = () => {
-
-
-  const { language } = useContext(UserContext)
+  const { language } = useContext(UserContext);
 
   return (
     <div className="home">
       <div className="home__container">
         <p>{language ? "Hello" : "Hola"}</p>
-        <h1>{language ? "I'm" : "Soy"} <span>Héctor</span></h1>
+        <h1>
+          {language ? "I'm" : "Soy"} <span>Héctor</span>
+        </h1>
         {/* <p>I consider myself to be hard working, willing to adapt in general with a great passion for learning, and keep improving as a Software Developer.</p> */}
-        {
-          language ? (
-            <p>I'm a Systems Engineer student with experience in the creation of web applications with front-end technologies. Passionate about the design and development of attractive and functional user interfaces, improving my skills in this field through various personal and academic projects, during my training, acquiring knowledge in HTML, CSS, and JavaScript, as well as in the use of frameworks such as ReactJs.</p>
-          ) : (
-            <p>Soy estudiante de Ingeniería en Sistemas con experiencia en la creación de aplicaciones web con tecnologías front-end. Me apasiona el diseño y desarrollo de interfaces de usuario atractivas y funcionales, mejorando mis habilidades en este campo a través de diversos proyectos personales y académicos, durante mi formación, adquiriendo conocimientos en HTML, CSS y JavaScript, así como en el uso de frameworks como ReactJs.</p>
-          )
-        }
+        {language ? (
+          <p>
+            I'm a Front-end Developer with experience in the creation of web
+            applications with front-end technologies. Passionate about designing
+            and developing attractive and functional user interfaces, improving
+            my skills in this field through various personal and academic
+            projects, and acquiring knowledge in HTML, CSS, JavaScript,
+            TypeScript, and ReactJs during my training.
+          </p>
+        ) : (
+          <p>
+            Soy Desarrollador Front-end con experiencia en la creación de
+            aplicaciones web con tecnologías front-end. Apasionado del diseño y
+            desarrollo de interfaces de usuario atractivas y funcionales,
+            mejorando mis habilidades en este campo a través de diversos
+            proyectos personales y académicos y adquiriendo conocimientos en
+            HTML, CSS, JavaScript, TypeScript y ReactJs durante mi formación.
+          </p>
+        )}
 
-        <div className='socials'>
+        <div className="socials">
           <Socials />
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
